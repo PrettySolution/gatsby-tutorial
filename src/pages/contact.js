c98159f4a6952a09b3c4aee377eafbd3256b9a2d
Vasyl Herman PC W10 <vasil.german@gmail.com>
2020-12-23 21:39:24 +0200

    favicon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Link} from "gatsby";
 import Header from "../components/header";
-
+import Container from "../components/container";
 export default function Contact(){
     return (
-        <div>
+        <Container>
             <Link to="/">Home</Link>
             <Header herderText="Contact" />
             <p>Send us a message!</p>
-        </div>
+        </Container>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import Header from "../components/header";
 import {Link} from "gatsby";
-
+import Container from "../components/container";
 export default function Home() {
   return (
-      <div style={{color: 'purple'}}>
-          <Link to="/contact/">Contact</Link>
-          <Header headerText="Hello Gatsby!2" />
+      <Container>
+          <Link to="/contact/">Contact</Link><br/>
+          <Link to="/about-css-modules/">about-css-modules</Link>
+          <Header headerText="Hello Gatsby!!!" />
           <p>What a world.</p>
           <img src="https://source.unsplash.com/random/400x200" alt=""/>
-      </div>
+      </Container>
   );
 }
