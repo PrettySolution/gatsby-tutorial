0a26152d9ac79f02016ffb7d741d40eeecc6ba52
Vasyl Herman PC W10 <vasil.german@gmail.com>
2020-12-24 14:54:07 +0200

    Updates

--- component---src-pages-about-js-e2df13076b02008bc57a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return a}));n("q1tI");var o=n("Wbzz"),i=n("Bl7J"),u=n("AeFk");function a(){var t=Object(o.c)("3159585216");return Object(u.b)(i.a,null,Object(u.b)("h1",null,"About ",t.site.siteMetadata.title),Object(u.b)("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-e2df13076b02008bc57a.js.map
--- component---src-pages-index-js-c69c854fd909ad19295a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(n,t,a){"use strict";a.r(t),a.d(t,"default",(function(){return e}));a("q1tI");var o=a("Bl7J"),b=a("AeFk");function e(){return Object(b.b)(o.a,null,Object(b.b)("h1",null,"Amazing Pandas Eating Things"),Object(b.b)("div",null,Object(b.b)("img",{src:"https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg",alt:"Group of pandas eating bamboo"})))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-c69c854fd909ad19295a.js.map
