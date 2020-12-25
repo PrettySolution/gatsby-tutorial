de81704295fb96c2c92d345407d087f9da493578
Vasyl Herman PC W10 <vasil.german@gmail.com>
2020-12-25 17:13:47 +0200

    blog styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { graphql, Link, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
+import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark {
         edges {
           node {
+            excerpt
             id
             frontmatter {
               title
@@ -28,8 +30,9 @@ const BlogPage = () => {
         {data.allMarkdownRemark.edges.map(edge => {
           return (
             <li key={edge.node.id}>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
+              <Link to={`/blog/${edge.node.fields.slug}`} className={blogStyles.link}>
               <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.excerpt}</p>
               <p>{edge.node.frontmatter.date}</p>
               </Link>
             </li>
